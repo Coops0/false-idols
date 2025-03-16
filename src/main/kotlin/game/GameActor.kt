99ac205512182403fb88ce3cbe6d9dd35ebb2033
Coops0 <51346812+Coops0@users.noreply.github.com>
2025-03-15 22:48:10 -0400

    2

--- src/main/kotlin/game/GameActor.kt
+++ src/main/kotlin/game/GameActor.kt
@@ -4,6 +4,7 @@ import com.cooper.message.InboundMessage
 import com.cooper.message.OutboundMessage
 import com.cooper.message.server.ServerInboundMessage
 import com.cooper.message.server.ServerOutboundMessage
+import com.cooper.message.server.UpdatePlayersServerOutboundMessage
 import kotlinx.coroutines.channels.Channel
 
 abstract class InnerApplicationMessage
@@ -20,32 +21,37 @@ class PlayerJoinInnerApplicationMessage(
     val callback: Channel<Result<SessionId>>
 ) : InnerApplicationMessage()
 
-class PlayerDisconnectInnerApplicationMessage(val sessionId: SessionId) : InnerApplicationMessage()
+class PlayerDisconnectInnerApplicationMessage(val playerName: PlayerName, val sessionId: SessionId) :
+        InnerApplicationMessage()
 
 val globalInnerApplicationChannel = Channel<InnerApplicationMessage>()
 
+class GameOverThrowable(val newState: GameState.GameOver) : Throwable()
+
 suspend fun launchGameActor(server: Channel<ServerOutboundMessage>) {
-    var gameState: GameState<*> = LobbyGameState(server)
+    var gameState: GameState = GameState.Lobby(server)
 
     for (message in globalInnerApplicationChannel) {
         when (message) {
             is PlayerInboundApplicationMessage -> {
-                gameState.handlePlayerInboundMessage(message.sessionId, message.inboundMessage)
+                try {
+//                gameState.handlePlayerInboundMessage(message.sessionId, message.inboundMessage)
+                } catch (e: GameOverThrowable) {
+                    gameState = e.newState
+                }
             }
 
             is ServerInboundApplicationMessage -> {
-                gameState.handleServerInboundMessage(message.serverInboundMessage)
+//                gameState.handleServerInboundMessage(message.serverInboundMessage)
             }
 
             is PlayerJoinInnerApplicationMessage -> gameState.handlePlayerJoin(message)
-            is PlayerDisconnectInnerApplicationMessage -> {
-                gameState.handlePlayerDisconnect(message.sessionId)
-            }
+            is PlayerDisconnectInnerApplicationMessage -> gameState.handlePlayerDisconnect(message)
         }
     }
 }
 
-private suspend fun GameState<*>.handlePlayerJoin(message: PlayerJoinInnerApplicationMessage) {
+private suspend fun GameState.handlePlayerJoin(message: PlayerJoinInnerApplicationMessage) {
     val existingPlayer = this.players.firstOrNull { it.name == message.playerName }
 
     val sessionId = "TODO" // todo generate
@@ -55,7 +61,7 @@ private suspend fun GameState<*>.handlePlayerJoin(message: PlayerJoinInnerApplic
         return
     }
 
-    if (this !is LobbyGameState) {
+    if (this !is GameState.Lobby) {
         message.callback.send(Result.failure(IllegalStateException("Cannot join game in progress")))
         return
     }
@@ -64,4 +70,14 @@ private suspend fun GameState<*>.handlePlayerJoin(message: PlayerJoinInnerApplic
     this.players.add(player)
 
     message.callback.send(Result.success(sessionId))
+}
+
+private suspend fun GameState.handlePlayerDisconnect(message: PlayerDisconnectInnerApplicationMessage) {
+    val player = this[message.playerName] ?: return
+    player.disconnect(message.sessionId)
+
+    if (this is GameState.Lobby && player.channels.isEmpty()) {
+        players.remove(player)
+        sendServer(UpdatePlayersServerOutboundMessage(players))
+    }
 }
--- src/main/kotlin/game/GameState.kt
+++ src/main/kotlin/game/GameState.kt
@@ -5,135 +5,177 @@ import com.cooper.message.server.ServerOutboundMessage
 import kotlinx.coroutines.channels.Channel
 import kotlin.math.absoluteValue
 
-abstract class GameState<P : Player>(
-    val name: String,
-    val server: Channel<ServerOutboundMessage>,
-    open val players: MutableList<P>
-) {
-    suspend fun broadcast(message: OutboundMessage) {
-        players.forEach { it.send(message) }
-    }
-
-    suspend fun send(playerName: PlayerName, message: OutboundMessage) {
-        players.firstOrNull { it.name == playerName }?.send(message)
-    }
+sealed class GameState {
+    abstract val name: String
+    abstract val server: Channel<ServerOutboundMessage>
+    abstract val players: List<Player>
 
     suspend fun sendServer(message: ServerOutboundMessage) {
         server.send(message)
     }
-}
 
-class LobbyGameState(server: Channel<ServerOutboundMessage>) : GameState<Player>("lobby", server, mutableListOf())
-
-class GameInProgressGameState private constructor(
-    server: Channel<ServerOutboundMessage>,
-    override val players: MutableList<GamePlayer>
-) : GameState<GamePlayer>("game_in_progress", server, players) {
-    constructor(previousState: GameState<Player>) : this(
-        previousState.server,
-        assignPlayerRoles(previousState.players)
-    )
-
-    var innerGameState: InnerGameState = AwaitingPlayerActionChoiceInnerGameState()
-    val deck: CardDeck = CardDeck()
-
-    var points: Int = 0
-        private set
-    var absolutePoints: Int = 0
-        private set
-
-    var failedElections: Int = 0
-
-    val chief: GamePlayer
-        get() = players.first(GamePlayer::isChief)
-
-    val satan: GamePlayer
-        get() = players.first { it.role == ComplexRole.SATAN }
-
-    val demons: List<GamePlayer>
-        get() = players.filter { it.role == ComplexRole.DEMON }
-
-    val angels: List<GamePlayer>
-        get() = players.filter { it.role == ComplexRole.ANGEL }
-
-    val alive: List<GamePlayer>
-        get() = players.filter(GamePlayer::isAlive)
-
-    /// 6+ cards played
-    val isLateGame: Boolean
-        get() = absolutePoints >= 6
-
-    fun addPoints(points: Int) {
-        this.points += points
-        this.absolutePoints += points.absoluteValue
+    operator fun get(playerName: PlayerName): Player? {
+        return players.firstOrNull { it.name == playerName }
     }
 
-    companion object {
-        abstract class InnerGameState(val name: String)
+    class Lobby(
+        override val server: Channel<ServerOutboundMessage>,
+        override val players: MutableList<Player> = mutableListOf()
+    ) : GameState() {
+        override val name: String = "lobby"
 
-        class AwaitingPlayerActionChoiceInnerGameState(
-            val forcedAction: ActionChoice? = null,
-            val cause: PlayerActionChoiceCause = PlayerActionChoiceCause.NORMAL_CHIEF,
-        ) : InnerGameState("awaiting_chief_action_choice") {
-            enum class PlayerActionChoiceCause {
-                NORMAL_CHIEF,
-                FORCED_CHIEF
-            }
+        suspend fun broadcast(message: OutboundMessage) {
+            players.forEach { it.send(message) }
         }
 
-        class AwaitingChiefCardDiscardInnerGameState(val cards: Array<Card>) :
-                InnerGameState("awaiting_chief_card_discard")
+        suspend fun send(playerName: PlayerName, message: OutboundMessage) {
+            players.firstOrNull { it.name == playerName }?.send(message)
+        }
 
-        class AwaitingAdvisorCardChoiceInnerGameState(val cards: Array<Card>) :
-                InnerGameState("awaiting_advisor_card_choice")
-
-        class AwaitingElectionOutcomeInnerGameState(val nominee: PlayerName) :
-                InnerGameState("awaiting_election_outcome")
-
-        private fun assignPlayerRoles(originalPlayers: List<Player>): MutableList<GamePlayer> {
-            val demonCount = when (originalPlayers.size) {
-                3, 4 -> 1
-                5, 6 -> 2
-                7, 8 -> 3
-                else -> 4
-            }
-
-            val shuffledPlayers = originalPlayers.shuffled().toMutableList()
-            val demons = List(demonCount) { shuffledPlayers.removeAt(0) }
-            val satan = shuffledPlayers.removeAt(0)
-
-            val players = mutableListOf<GamePlayer>()
-
-            players.add(GamePlayer(satan, ComplexRole.SATAN))
-            players.addAll(demons.map { GamePlayer(it, ComplexRole.DEMON) })
-            players.addAll(shuffledPlayers.map { GamePlayer(it, ComplexRole.ANGEL) })
-
-            return players.shuffled().toMutableList()
+        fun toGameInProgress(): GameInProgress {
+            return GameInProgress(server, players)
         }
     }
-}
 
-class GameOverGameState(
-    server: Channel<ServerOutboundMessage>,
-    players: MutableList<Player>,
-    val winner: SimpleRole,
-    val satan: PlayerName,
-    val demons: List<PlayerName>,
-    val cause: GameOverCause
-) : GameState<Player>("game_over", server, players) {
-    constructor(gameInProgress: GameInProgressGameState, winner: SimpleRole, cause: GameOverCause) : this(
-        gameInProgress.server,
-        players = gameInProgress.players.toMutableList(),
-        winner,
-        satan = gameInProgress.satan.name,
-        demons = gameInProgress.demons.map(GamePlayer::name),
-        cause
-    )
+    class GameInProgress private constructor(
+        override val server: Channel<ServerOutboundMessage>,
+        override val players: MutableList<GamePlayer>
+    ) : GameState() {
+        override val name: String = "game_in_progress"
+
+        constructor(server: Channel<ServerOutboundMessage>, originalPlayers: List<Player>) : this(
+            server,
+            assignPlayerRoles(originalPlayers)
+        )
+
+        var innerGameState: InnerGameState = InnerGameState.AwaitingPlayerActionChoice()
+        val deck: CardDeck = CardDeck()
+
+        var points: Int = 0
+            private set
+        var absolutePoints: Int = 0
+            private set
+
+        var failedElections: Int = 0
+
+        val chief: GamePlayer
+            get() = players.first(GamePlayer::isChief)
+
+        val satan: GamePlayer
+            get() = players.first { it.role == ComplexRole.SATAN }
+
+        val demons: List<GamePlayer>
+            get() = players.filter { it.role == ComplexRole.DEMON }
+
+        val angels: List<GamePlayer>
+            get() = players.filter { it.role == ComplexRole.ANGEL }
+
+        val alive: List<GamePlayer>
+            get() = players.filter(GamePlayer::isAlive)
+
+        /// 6+ cards played
+        val isLateGame: Boolean
+            get() = absolutePoints >= 6
+
+        fun addPoints(points: Int) {
+            this.points += points
+            this.absolutePoints += points.absoluteValue
+        }
+
+        suspend fun broadcast(message: OutboundMessage) {
+            players.forEach { it.send(message) }
+        }
+
+        suspend fun send(playerName: PlayerName, message: OutboundMessage) {
+            players.firstOrNull { it.name == playerName }?.send(message)
+        }
+
+        fun toGameOver(winner: SimpleRole, cause: GameOverCause): GameOver {
+            return GameOver(
+                server,
+                players.toMutableList(),
+                winner,
+                satan.name,
+                demons.map(GamePlayer::name),
+                cause
+            )
+        }
+
+        private companion object {
+            private fun assignPlayerRoles(originalPlayers: List<Player>): MutableList<GamePlayer> {
+                val demonCount = when (originalPlayers.size) {
+                    3, 4 -> 1
+                    5, 6 -> 2
+                    7, 8 -> 3
+                    else -> 4
+                }
+
+                val shuffledPlayers = originalPlayers.shuffled().toMutableList()
+                val demons = List(demonCount) { shuffledPlayers.removeAt(0) }
+                val satan = shuffledPlayers.removeAt(0)
+
+                val players = mutableListOf<GamePlayer>()
+
+                players.add(GamePlayer(satan, ComplexRole.SATAN))
+                players.addAll(demons.map { GamePlayer(it, ComplexRole.DEMON) })
+                players.addAll(shuffledPlayers.map { GamePlayer(it, ComplexRole.ANGEL) })
+
+                return players.shuffled().toMutableList()
+            }
+        }
+    }
+
+    class GameOver(
+        override val server: Channel<ServerOutboundMessage>,
+        override val players: MutableList<Player>,
+        val winner: SimpleRole,
+        val satan: PlayerName,
+        val demons: List<PlayerName>,
+        val cause: GameOverCause
+    ) : GameState() {
+        override val name: String = "game_over"
+
+        suspend fun broadcast(message: OutboundMessage) {
+            players.forEach { it.send(message) }
+        }
+
+        suspend fun send(playerName: PlayerName, message: OutboundMessage) {
+            players.firstOrNull { it.name == playerName }?.send(message)
+        }
+    }
 
     enum class GameOverCause {
         SATAN_KILLED,
         SATAN_ELECTED,
         POSITIVE_THRESHOLD_REACHED,
         ALL_ANGELS_DEAD,
+    }
+}
+
+sealed class InnerGameState {
+    abstract val name: String
+
+    class AwaitingPlayerActionChoice(
+        val forcedAction: ActionChoice? = null,
+        val cause: PlayerActionChoiceCause = PlayerActionChoiceCause.NORMAL_CHIEF,
+    ) : InnerGameState() {
+        override val name: String = "awaiting_chief_action_choice"
+
+        enum class PlayerActionChoiceCause {
+            NORMAL_CHIEF,
+            FORCED_CHIEF
+        }
+    }
+
+    class AwaitingChiefCardDiscard(val cards: Array<Card>) : InnerGameState() {
+        override val name: String = "awaiting_chief_card_discard"
+    }
+
+    class AwaitingAdvisorCardChoice(val cards: Array<Card>) : InnerGameState() {
+        override val name: String = "awaiting_advisor_card_choice"
+    }
+
+    class AwaitingElectionOutcome(val nominee: PlayerName) : InnerGameState() {
+        override val name: String = "awaiting_election_outcome"
     }
 }
