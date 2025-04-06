111f9a2a24c4188da4ee10c13e0fd20fa9e0cb65
Coops0 <51346812+Coops0@users.noreply.github.com>
2025-04-06 15:06:13 -0400

    optimize player icons

--- misc/board-tracker/src/components/DemonBoard.vue
+++ misc/board-tracker/src/components/DemonBoard.vue
@@ -1,10 +1,15 @@
 <template>
-
+  <div class="flex w-full h-[270px] relative items-center">
+    <img class="object-scale-down" :src="boardImage" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { Card } from '@/App.vue';
 import { computed } from 'vue';
+import Board56Image from '@/assets/board/board-demon-5-6.png';
+import Board78Image from '@/assets/board/board-demon-7-8.png';
+import Board910Image from '@/assets/board/board-demon-9-10.png';
 
 const props = defineProps<{
   cards: Card[];
@@ -14,14 +19,20 @@ const props = defineProps<{
 const negativeCards = computed(() => props.cards.filter(card => card.consequence === 'NEGATIVE'));
 const boardVariant = computed<1 | 2 | 3>(() => {
   const s = props.playersSize;
-  if (s === 4 || s === 5 || s === 6) {
-    return 1;
-  }
-
-  if (s === 7 || s === 8) {
-    return 2;
-  }
-
+  if (s === 4 || s === 5 || s === 6) return 1;
+  if (s === 7 || s === 8) return 2;
   return 3;
+});
+
+// all 1601 x 539
+const boardImage = computed(() => {
+  switch (boardVariant.value) {
+    case 1:
+      return Board56Image;
+    case 2:
+      return Board78Image;
+    case 3:
+      return Board910Image;
+  }
 });
 </script>
